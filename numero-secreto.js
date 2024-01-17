c58822372e681f75a71b2a4c06f0877b88d7ae7f
Estefani Reyes Ortiz <147439990+Estefani-Reyes@users.noreply.github.com>
2024-01-17 19:50:54 -0400

    v.0 numero-secreto.js

--- numero-secreto.js
+++ numero-secreto.js
@@ -1,21 +1,27 @@
-alert('Bienvenido al juego del número secreto');
+document.write('Adivina el número secreto')
 let intento = prompt('Elige un número entre 1 y 10');
-
-while(isNaN(intento)){
-    intento = prompt('Elige un número entre 1 y 10');
+validacion (intento)
+function validacion (intento){
+    while(isNaN(intento)){
+        intento = prompt('Elige un número entre 1 y 10');
+    }
+return intento;
 }
-let numeroSecreto = 4;
-for(i = intento; 1 <= 10; i++){
 
-    console.log(intento  == numeroSecreto);
+let numeroSecreto = 4;
+for(i =  validacion (intento); 1 <= 10; i++){
+
     if(i <= 10 & i > 1){
         if (intento  == numeroSecreto) {
             alert('Felicidades Acertaste!');
+            break;
         } else {
             alert('El número secreto era ' + numeroSecreto);
+            break;
         }
     }else{
         alert('ESTAS COLOCANDO UN NUMERO QUE NO ESTA EN EL RANGO SOLICITADO'); 
+        break;
     }
 
 }
