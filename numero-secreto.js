8a291d10e9d2e4b8272b995f0d1691ec1d624179
Estefani Reyes <estefani.r.20x@gmail.com>
2024-01-17 17:41:16 -0400

    v-0

---
+++ numero-secreto.js
@@ -0,0 +1,21 @@
+alert('Bienvenido al juego del número secreto');
+let intento = prompt('Elige un número entre 1 y 10');
+
+while(isNaN(intento)){
+    intento = prompt('whiler Elige un número entre 1 y 10');
+}
+let numeroSecreto = 4;
+for(i = intento; 1 <= 10; i++){
+
+    console.log(intento  == numeroSecreto);
+    if(i <= 10 & i > 1){
+        if (intento  == numeroSecreto) {
+            alert('Felicidades Acertaste!');
+        } else {
+            alert('El número secreto era ' + numeroSecreto);
+        }
+    }else{
+        alert('ESTAS COLOCANDO UN NUMERO QUE NO ESTA EN EL RANGO SOLICITADO'); 
+    }
+
+}
